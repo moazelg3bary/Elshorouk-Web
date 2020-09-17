7cd06893de43ed650a2a5825ff821e08e3032099
moaz_home <moazHome@gmail.com>
2020-09-17 15:25:40 +0200

    add slider js

--- file.js/app.js
+++ file.js/app.js
@@ -1,4 +1,22 @@
 /*global console, alert, swal, prompt, $*/
 window.onload = function () {
   'use strict';
+  var container = document.getElementById('view_img'),
+      children = document.getElementById('children');
+
+  for(let i = 0; i < children.children.length; i++) {
+
+    children.children[i].onclick = function() {
+
+      container.firstElementChild.setAttribute('src', this.firstElementChild.getAttribute('src'));
+
+      for (let i = 0; i < this.parentElement.children.length; i++) {
+
+        this.parentElement.children[i].classList.remove('active')
+
+      }
+      this.classList.add('active')
+    }
+  }
+  
 }
