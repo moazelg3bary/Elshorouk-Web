dc7c65ba4a6d1c6b4c6fa86ec476351bfdb94413
moaz_home <moazHome@gmail.com>
2020-10-07 21:35:46 +0200

    fix some pugs

--- file.js/app.js
+++ file.js/app.js
@@ -10,22 +10,31 @@ window.onload = function () {
       children = document.getElementById('children'),
       total_cart = document.querySelector('.total-cart'),
       ditlist = document.querySelectorAll('.ditlist'),
-      notification = document.querySelector('.notification');
+      heart = document.querySelectorAll('.heart'),
+      notification_cart = document.querySelector('.notification-cart'),
+      notification_heart = document.querySelector('.notification-heart');
 
 
 
   // get cart & increase number
   for (let i = 0; i < ditlist.length; i++) {
-    ditlist[i].lastElementChild.onclick = function () {
-      toNum = parseInt(total_cart.innerHTML);
-      calc = toNum += 1;
-      total_cart.innerHTML = calc;
+      ditlist[i].lastElementChild.onclick = function () {
+        toNum = parseInt(total_cart.innerHTML);
+        calc = toNum += 1;
+        total_cart.innerHTML = calc;
 
-      notification.classList.add('show')
-      
-      notification.classList.contains('show') ? clear = setTimeout(function () { notification.classList.remove('show') }, 3000) : null
+        notification_cart.classList.add('show')
+        
+        notification_cart.classList.contains('show') ? clear = setTimeout(function () { notification_cart.classList.remove('show') }, 3000) : null
     }
+    // heart[i].onclick = function() {
 
+    //   this.classList.contains('active') ? this.classList.remove('active') : this.classList.add('active')
+
+    //   notification_heart.classList.add('show')
+      
+    //   notification_heart.classList.contains('show') ? clear = setTimeout(function () { notification_heart.classList.remove('show') }, 3000) : null
+    // }
   }      
 
   
@@ -33,13 +42,14 @@ window.onload = function () {
 
     children.children[i].onclick = function() {
 
-      container.firstElementChild.setAttribute('src', this.firstElementChild.getAttribute('src'));
+      // container.firstElementChild.setAttribute('src', this.firstElementChild.getAttribute('src'));
 
       for (let i = 0; i < this.parentElement.children.length; i++) {
-
+        container.children[i].classList.remove('active')
         this.parentElement.children[i].classList.remove('active')
 
       }
+      container.children[i].classList.add('active')
       this.classList.add('active')
     }
   }
